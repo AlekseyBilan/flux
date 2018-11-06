84d4cd83083a78ee1d1af66de4856afce2365c52
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2018-11-06 22:03:46 +0200

    fix cors

--- simple_api/api/index.js
+++ simple_api/api/index.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var mocks = require('./mock');
 var assign = require('object-assign');
 
-router.get('/article', function (req, res, next) {
+router.get('/articles', function (req, res, next) {
     var articles = withComments(mocks.articles).map(function (article) {
             return assign({}, article, {
                 text: undefined
--- simple_api/server.js
+++ simple_api/server.js
@@ -7,8 +7,14 @@ var port = 3001;
 var app = express();
 
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/api', api);
-
 app.listen(port, 'localhost', function (err) {
     if (err) {
         console.log(err);
